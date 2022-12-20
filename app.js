188d8015790f727439454ce58a56c85451ba9970
josue4433 <113935346+josue4433@users.noreply.github.com>
2022-12-20 09:36:38 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const PI = 3.14;
+console.log(PI);
+
+PI = 3;
+console.log(PI);
+
+const PI = 4;
+console.log(PI);
+
+var PI = 5;
+console.log(PI);
+
+
+//1.What is the difference between var and let?
+//var is function scoped and let is block scoped.
+
+//2.What is the difference between var and const?
+//You can reassign and redeclare with var, but you can not redeclare or reassign using the const keyword. You can access a hoisted variable with var. Const creates block scope.
+
+//3.What is the difference between let and const?
+//You can reassign with let, but you can not redeclare or reassign using the const keyword.
+
+//4.What is hoisting?
+
+//It’s the way that we explain how the JS compiler works. Variables are lifting or “hoisted” to the top of the scope they are declared in. When using var, you can access the variable name and it’s undefined value before it is used later on.
+
