d4f596b038a625d2c81c45244fc6f1107568edb1
josue4433 <113935346+josue4433@users.noreply.github.com>
2022-12-21 09:49:05 -0500

    Add files via upload
    14.5 Object Enhacements

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-const filterOutOdds = (...arguments) =>
-arguments.filter((num) => num % 2 ===0);
-
-
-const numeros = [2, 4, 6, 8, 10, 1, 1.25]
-const findMin = (...nums) => {
-    return nums.reduce((min, next) => min < next ? min : next)
+function createInstuctor (firstName, lastName){
+    return {
+        firstName,
+        lastName,
+    }
 }
-console.log(findMin(...numeros));
 
-const firstList = {a:1, b:2};
-const secondList = {c:3, d:4};
-const mergeObjects = (a, b) => ({...a, ...b}); // need parenthesis, otherwise error
-console.log(mergeObjects(firstList, secondList));
+let favoriteNumber = 7;
+const instuctor = {
+    firstName: "Josue",
+    [favoriteNumber]: "THis is my favorite number"
+}
 
+const c = createAnimals("Cat", "Meoww" , "mmmm")
+c.meoww()
 
-const doubleAndReturnArgs = (arr, ...more) => {
-    const double = more.map((v) => v * 2);
-    return [...arr, ...double];
-  }
-  console.log(doubleAndReturnArgs([1,2,3],4,4));
-  console.log(doubleAndReturnArgs([2],10,4));
-  
-  
+const h = createAnimals("Hores", "OOOO", "Btt")
+h.ooo()
+
+function createAnimals(Lizard , poisons, hissing) {
+    return {
+        Lizard,
+        [poisons](){
+            return hissing;
+        }
+    }
+}
