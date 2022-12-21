3d36eeeaeb1254f48fc12065293e34ebc0df7e39
josue4433 <113935346+josue4433@users.noreply.github.com>
2022-12-21 11:03:23 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-function double(arr) {
-    return arr.map(function(val) {
-    return val * 2;
-    });
-}
-
-const double = arr => arr.map(val * 2);
-
-
-function squareAndFindEvens(numbers) {
-    var squares = number.map(function(num){
-        return num **2;
-    });
-    var evens = squares.filter(function(square){
-        return square %2 === 0;
-    });
-    return evens;
-}
-const squareAndFindEvens = number => numbers.map(val => val ** 2).filter(square => square % 2 ===0)
+raceResults(['Miller' , 'Kane' , 'Messi', 'Ronaldo' , 'Carmen'])
+//{first: "Miller", second: "Kane" , third: "Messi" , rest: ["Ronaldo" , "Carmen"]}
+const raceResults = ([first, second, third, ...rest]) => ({first, second, third, rest})
